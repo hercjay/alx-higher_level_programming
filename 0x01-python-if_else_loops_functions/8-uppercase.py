8908862cb658dd81251f1cce0fc8e7113d7188a4
Michael Anokwulu <112480000+hercjay@users.noreply.github.com>
2023-03-07 14:35:34 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,14 +2,9 @@
 # Author - Michael Hercjay Anokwulu
 
 def uppercase(s):
-    for c in s:
-        # Check if character is lowercase
-        if ord('a') <= ord(c) <= ord('z'):
-            # Convert lowercase character to uppercase using ASCII code
-            # and print without new line
-            print(chr(ord(c) - ord('a') + ord('A')), end='')
-        else:
-            # Print character without modification and without new line
-            print(c, end='')
-    # Print new line using second print function
-    print()
+    for i in s:
+        c = ord(i)
+        if c >= 97 and c <= 122:
+            c -= 32
+        print("{:c}".format(c), end="")
+    print("")
