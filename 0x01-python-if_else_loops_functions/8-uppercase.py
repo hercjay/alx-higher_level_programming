1a0fa8be5fc2657fc75e47eba02b2d0f31596d9b
Michael Anokwulu <112480000+hercjay@users.noreply.github.com>
2023-03-07 14:30:43 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,8 @@
 
 def uppercase(s):
     for c in s:
-        print(chr(ord(c) - 32), end="")
+        if ord(c) >= ord('a') and ord(c) <= ord('z'):
+            print(chr(ord(c) - 32), end="")
+        else:
+            print(c, end="")
     print()
