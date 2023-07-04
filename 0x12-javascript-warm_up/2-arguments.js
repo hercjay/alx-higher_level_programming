ee0da8d68989bf63da871ff1b6840fb826ec03a8
Michael Anokwulu <112480000+hercjay@users.noreply.github.com>
2023-07-04 10:54:15 -0700

    py fil

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,7 @@
 #!/usr/bin/node
-
-const len = process.argv.length;
-
-if (len < 3) {
+if (process.argv.length === 2) {
   console.log('No argument');
-} else if (len === 3) {
+} else if (process.argv.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
